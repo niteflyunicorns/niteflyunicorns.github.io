98c5b865f1d55382dbdde464936c3c1f187187cf
Savannah Chappus <savannahj1200@gmail.com>
2025-10-13 11:48:26 -0700

    fixed wrong directory reference

--- assets/js/generateProjects.js
+++ assets/js/generateProjects.js
@@ -163,7 +163,7 @@ const projects = [
 	role: "All",
 	soft: "Procreate, Adobe After Effects",
 	desc: "This is a title sequence that I designed based on the legend of the Loch Ness monster. The lochness monster scenes are all hand drawn frame by frame in Procreate and overlayed onto text in Adobe After Effects.",
-	dir: 'ShortNarrative'
+	dir: 'Lochness'
     },
     {
 	id: 'best-friends',
